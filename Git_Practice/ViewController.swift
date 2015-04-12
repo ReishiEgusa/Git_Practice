a3227ab653b8507f4bc572ce6658438d2f1bfc33
Reishi.E <Reishi@ReishiE-no-MacBook-Air.local>
2015-04-12 14:36:39 +0900

    Initial Commit

---
+++ Git_Practice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Git_Practice
+//
+//  Created by Reishi.E on 2015/04/12.
+//  Copyright (c) 2015年 Reishi.E. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
